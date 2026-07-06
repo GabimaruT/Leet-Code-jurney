d9420103b4f57cb2906ba15b2088be9a296968e6
GabimaruT <krishnawalje092@gmail.com>
2026-07-07 01:49:32 +0530

    0 ms (100%) 44 MB (43.15%) 07-07-2026 0074-search-a-2d-matrix .java Medium Array

--- Java/Medium/0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ Java/Medium/0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -3,22 +3,25 @@ class Solution {
         int row = matrix.length;
         int col = matrix[0].length;
 
-        int i = 0;
-        int j = col - 1;
+        int low = 0;
+        int high = (row * col) - 1;
 
-        while(i < row && j >=0)
+        while(low <= high)
         {
-            if(matrix[i][j] == target)
+            int mid = low + (high - low) / 2;
+
+            if(matrix[mid / col][mid % col] == target)
             {
                 return true;
             }
-            else if(matrix[i][j] > target)
+
+            else if(matrix[mid / col][mid % col] > target)
             {
-                j--;
+                high = mid - 1;
             }
             else
             {
-                i++;
+                low = mid + 1;
             }
         }
         return false;
