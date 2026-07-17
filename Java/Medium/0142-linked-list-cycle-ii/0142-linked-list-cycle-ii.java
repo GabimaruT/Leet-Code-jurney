90b12a380263bd02088b471294b3b4326cbce6e5
GabimaruT <krishnawalje092@gmail.com>
2026-07-18 03:12:37 +0530

    0 ms (100%) 46.7 MB (50.21%) 07-18-2026 0142-linked-list-cycle-ii .java Medium Hash Table

--- Java/Medium/0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ Java/Medium/0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -11,24 +11,20 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-           if(head == null)
-        {
-            return null;
-        }
+        
+        if(head == null) return null;
 
-        ListNode fast =head;
         ListNode slow = head;
+        ListNode fast = head;
 
-        while(fast != null && fast.next != null )
+        while(fast != null && fast.next != null)
         {
             fast = fast.next.next;
             slow = slow.next;
 
-            if(fast == slow)
-            {
-                break;
-            }
+            if(fast == slow) break;
         }
+
         if(fast == null || fast.next == null) return null;
 
         while(head != slow)
@@ -36,6 +32,7 @@ public class Solution {
             head = head.next;
             slow = slow.next;
         }
+        
         return head;
     }
 }
