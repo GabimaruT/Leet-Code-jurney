638ab4c8aa713b7061e0471712712840e3900a87
GabimaruT <krishnawalje092@gmail.com>
2026-07-24 02:21:45 +0530

    9 ms (98.57%) 59.5 MB (37.28%) 07-24-2026 0148-sort-list .java Medium Linked List

--- Java/Medium/0148-sort-list/0148-sort-list.java
+++ Java/Medium/0148-sort-list/0148-sort-list.java
@@ -11,32 +11,59 @@
 class Solution {
     public ListNode sortList(ListNode head) {
         
-        ListNode temp = head;
-        int size = 0;
-        while(temp != null)
+        return divide(head);
+    }
+
+    static ListNode divide(ListNode head)
+    {
+        if(head == null || head.next == null) return head;
+
+        ListNode mid = findMid(head);
+        ListNode left = head;
+        ListNode right = mid.next;
+        mid.next = null;
+
+        left = divide(left);
+        right = divide(right);
+
+        return conqure(left, right);
+    }
+
+    static ListNode findMid(ListNode head)
+    {
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while(fast != null && fast.next != null)
         {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+
+    static ListNode conqure(ListNode left, ListNode right)
+    {
+        ListNode dummy = new ListNode();
+        ListNode temp = dummy;
+
+        while(left != null && right != null)
+        {
+            if(left.val < right.val)
+            {
+                temp.next = left;
+                left = left.next;
+            }
+            else
+            {
+                temp.next = right;
+                right = right.next;
+            }
             temp = temp.next;
-            size++;
         }
 
-        int arr[] = new int[size];
-        temp = head;
-
-        for(int i=0; i<size; i++)
-        {
-            arr[i] = temp.val;
-            temp = temp.next;
-        }
-
-        Arrays.sort(arr);
-        temp = head;
-
-         for(int i=0; i<size; i++)
-        {
-            temp.val = arr[i];
-            temp = temp.next;
-        }
-
-        return head;
+        if (left != null) temp.next = left;
+        if (right != null) temp.next = right;
+        return dummy.next;
     }
 }
