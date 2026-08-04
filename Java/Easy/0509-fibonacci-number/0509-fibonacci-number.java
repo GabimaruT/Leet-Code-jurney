fd2ee3d4bd1eabd2bb3dd449d13a419e5042ec1e
GabimaruT <krishnawalje092@gmail.com>
2026-08-05 03:06:30 +0530

    0 ms (100%) 42.3 MB (16.06%) 08-05-2026 0509-fibonacci-number .java Easy Math

--- Java/Easy/0509-fibonacci-number/0509-fibonacci-number.java
+++ Java/Easy/0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,16 +1,17 @@
 class Solution {
     public int fib(int n) {
         
-        int dp[] = new int[n+1];
-        Arrays.fill(dp,-1);
-        return solve(n, dp);
-    }
-    static int solve(int n, int dp[])
-    {
-        if(n <=1) return n;
+        if(n <= 1) return n;
 
-        if(dp[n] != -1) return dp[n];
+        int prev2 = 0;
+        int prev1 = 1;
 
-        return dp[n] = solve(n-1, dp) + solve(n-2, dp);
+        for(int i=2; i<=n; i++)
+        {
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
     }
 }
