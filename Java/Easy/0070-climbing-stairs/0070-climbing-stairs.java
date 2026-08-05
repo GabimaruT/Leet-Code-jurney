25fb1b15db0e0d67e9bf1c4730a08f3b930fb01a
GabimaruT <krishnawalje092@gmail.com>
2026-08-06 02:15:53 +0530

    0 ms (100%) 41.9 MB (73.52%) 08-06-2026 0070-climbing-stairs .java Easy Math

--- Java/Easy/0070-climbing-stairs/0070-climbing-stairs.java
+++ Java/Easy/0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,16 +1,18 @@
 class Solution {
     public int climbStairs(int n) {
 
-        int dp[] = new int[n+1];
-        Arrays.fill(dp, -1);
-        return soln(dp, n);
-    }
-    static int soln(int dp[],int n)
-    {
-        if(n == 0 || n == 1) return 1;
+        if(n <= 3) return n;
 
-        if(dp[n] != -1) return dp[n];
+        int prev1 = 3;
+        int prev2 = 2;
+        int curr = 0;
 
-        return dp[n] = soln(dp, n - 1) + soln(dp, n - 2);
+        for(int i=3; i<n; i++)
+        {
+            curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
     }
 }
