2d20d46a9127b5dfc9a0c771601e6cd334cc6b07
GabimaruT <krishnawalje092@gmail.com>
2026-08-13 00:51:31 +0530

    0 ms (100%) 42.5 MB (70.81%) 08-13-2026 0342-power-of-four .java Easy Math

--- Java/Easy/0342-power-of-four/0342-power-of-four.java
+++ Java/Easy/0342-power-of-four/0342-power-of-four.java
@@ -2,10 +2,8 @@ class Solution {
     public boolean isPowerOfFour(int n) {
         
         if(n < 1) return false;
-
-        else
-        {
-            return Math.log(n) / Math.log(4) % 1 == 0;
-        }
+        if(n == 1) return true;
+        if(n % 4 != 0) return false;
+        return isPowerOfFour(n/4);
     }
 }
