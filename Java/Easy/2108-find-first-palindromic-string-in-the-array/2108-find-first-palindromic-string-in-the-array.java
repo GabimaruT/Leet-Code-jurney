5d0b52e9acb9c9d2c4aa2835fa527c88e5c025dc
GabimaruT <krishnawalje092@gmail.com>
2026-08-14 01:27:14 +0530

    1 ms (100%) 47.5 MB (50.13%) 08-14-2026 2108-find-first-palindromic-string-in-the-array .java Easy Array

---
+++ Java/Easy/2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String firstPalindrome(String[] words) {
+
+        for(String word : words)
+        {
+            if(isPalendrom(word))
+            {
+                return word;
+            }
+        }
+        return "";
+    }
+
+    static boolean isPalendrom(String s)
+    {
+        int i = 0;
+        int j = s.length() - 1;
+
+        while(i < j)
+        {
+            if(s.charAt(i) != s.charAt(j)) 
+            {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
