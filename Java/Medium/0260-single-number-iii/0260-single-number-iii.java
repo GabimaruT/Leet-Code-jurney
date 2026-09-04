27ac9def788ff0bf2e415852284abbc7db0fac5c
GabimaruT <krishnawalje092@gmail.com>
2026-09-05 03:06:38 +0530

    1 ms (100%) 48.1 MB (22.23%) 09-05-2026 0260-single-number-iii .java Medium Array

--- Java/Medium/0260-single-number-iii/0260-single-number-iii.java
+++ Java/Medium/0260-single-number-iii/0260-single-number-iii.java
@@ -1,27 +1,33 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
         
+        int xor = 0;
         int res[] = new int[2];
+
         for(int i=0; i<nums.length; i++)
         {
-            for(int j=i+1; j<nums.length; j++)
+            xor = xor^nums[i];
+        }
+        int rightMost = xor & -xor; 
+
+        int b1 = 0;
+        int b2 = 0; 
+
+        for(int i=0; i<nums.length; i++)
+        {
+            if((nums[i] & rightMost) != 0)
             {
-                if((nums[i]^nums[j]) == 0)
-                {
-                    nums[i] = 0;
-                    nums[j] = 0;
-                }
+                b1 = b1^nums[i];
+            }
+            else
+            {
+                b2 = b2^nums[i];
             }
         }
 
-        for(int i=0,j=0; i<nums.length; i++)
-        {
-            if(nums[i] != 0 )
-            {
-                res[j] = nums[i];
-                j++;
-            }
-        }
+        res[0] = b1;
+        res[1] = b2;
+
         return res;
     }
 }
