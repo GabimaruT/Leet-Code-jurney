46b15f9691631e5e66cd7e7d6da95705a3ea5c9f
GabimaruT <krishnawalje092@gmail.com>
2026-09-14 03:31:33 +0530

    0 ms (100%) 42.8 MB (51.47%) 09-14-2026 0232-implement-queue-using-stacks .java Easy Stack

--- Java/Easy/0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ Java/Easy/0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -1,47 +1,46 @@
 class MyQueue {
-
-    private Stack<Integer> s1;
-    private Stack<Integer> s2;
+    Stack<Integer> s1 = new Stack<>();
+    Stack<Integer> s2 = new Stack<>();
 
     public MyQueue() {
-        
-        s1 = new Stack<>();
-        s2 = new Stack<>();
     }
-    
+
     public void push(int x) {
-        
-        while(!s1.isEmpty())
-        {
-            s2.push(s1.peek());
-            s1.pop();
-        }
-
         s1.push(x);
+    }
 
-        while(!s2.isEmpty())
-        {
-            s1.push(s2.peek());
-            s2.pop();
-        }
-    }
-    
     public int pop() {
-        
-        return s1.pop();
+        while (!s1.isEmpty()) {
+            s2.push(s1.pop());
+        }
+
+        int ans = s2.pop();
+
+        while (!s2.isEmpty()) {
+            s1.push(s2.pop());
+        }
+
+        return ans;
     }
-    
+
     public int peek() {
-        
-        return s1.peek();
+        while (!s1.isEmpty()) {
+            s2.push(s1.pop());
+        }
+
+        int ans = s2.peek();
+
+        while (!s2.isEmpty()) {
+            s1.push(s2.pop());
+        }
+
+        return ans;
     }
-    
+
     public boolean empty() {
-        
-        return s1.isEmpty();
+        return s1.isEmpty() && s2.isEmpty();
     }
 }
-
 /**
  * Your MyQueue object will be instantiated and called as such:
  * MyQueue obj = new MyQueue();
