7e07a3bf3f782cedcad25edaa58854659ed73066
GabimaruT <krishnawalje092@gmail.com>
2026-09-22 03:37:46 +0530

    2 ms (99.48%) 45.5 MB (10.67%) 09-22-2026 0496-next-greater-element-i .java Easy Array

--- Java/Easy/0496-next-greater-element-i/0496-next-greater-element-i.java
+++ Java/Easy/0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,28 +1,24 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int[] result = new int[nums1.length];
         
-        for (int i = 0; i < nums1.length; i++) {
-            int target = nums1[i];
-            int nextGreater = -1;
-            boolean found = false;
-            
-            for (int j = 0; j < nums2.length; j++) {
-                // Find target in nums2 first
-                if (nums2[j] == target) {
-                    found = true;
-                }
-                
-                // Once target is found, look for the first greater element to its right
-                if (found && nums2[j] > target) {
-                    nextGreater = nums2[j];
-                    break; // Stop at the very first greater element
-                }
+        Stack<Integer> st = new Stack<>();
+        int nextGratest[] = new int[10001];
+
+        for(int j=nums2.length-1; j>=0; j--)
+        {
+            while(!st.isEmpty() && st.peek() <=nums2[j])
+            {
+                st.pop();
             }
-            
-            result[i] = nextGreater;
+
+            nextGratest[nums2[j]] = st.isEmpty() ? -1 : st.peek();
+            st.push(nums2[j]);
         }
-        
-        return result;
+
+        for(int i=0; i<nums1.length; i++)
+        {
+            nums1[i] = nextGratest[nums1[i]];
+        }
+        return nums1;
     }
 }
