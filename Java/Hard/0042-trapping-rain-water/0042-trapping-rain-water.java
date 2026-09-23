4e96976c8707bbfc80db7eecd5e9786743bafd60
GabimaruT <krishnawalje092@gmail.com>
2026-09-24 03:52:54 +0530

    0 ms (100%) 48 MB (35.9%) 09-24-2026 0042-trapping-rain-water .java Hard Array

--- Java/Hard/0042-trapping-rain-water/0042-trapping-rain-water.java
+++ Java/Hard/0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,45 +1,39 @@
 class Solution {
-    public int trap(int[] height) {
+    public int trap(int[] nums) {
         
-        int totalWater = 0;
-        int prefixMax[] = findPrefixMax(height);
-        int sufixMax[] = findSufixMax(height);
+        int leftMax = 0;
+        int rightMax = 0;
+        int total = 0;
+        int l = 0;
+        int r = nums.length-1;
 
-        for(int i=0; i<height.length; i++)
+        while(l < r)
         {
-            if(height[i] < prefixMax[i] && height[i] < sufixMax[i])
+            if(nums[l] <= nums[r])
             {
-                totalWater += Math.min(prefixMax[i],sufixMax[i]) - height[i];
+                if(leftMax > nums[l])
+                {
+                    total += leftMax - nums[l];
+                }
+                else
+                {
+                    leftMax = nums[l];
+                }
+                l++;
+            }
+            else
+            {
+                if(rightMax > nums[r])
+                {
+                    total += rightMax - nums[r];
+                }
+                else
+                {
+                    rightMax = nums[r];
+                }
+                r--;
             }
         }
-
-        return totalWater;
-
-    }
-
-    static int[] findPrefixMax(int [] height)
-    {
-        int prefixMax[] = new int[height.length];
-        prefixMax[0] = height[0];
-
-        for(int i=1; i<height.length; i++)
-        {
-            prefixMax[i] = Math.max(prefixMax[i-1], height[i]);
-        }
-
-        return prefixMax;
-    }
-
-    static int[] findSufixMax(int[] height)
-    {
-        int sufixMax[] = new int[height.length];
-        sufixMax[height.length-1] = height[height.length-1];
-
-        for(int i=height.length-2; i>=0; i--)
-        {
-            sufixMax[i] = Math.max(sufixMax[i+1],height[i]);
-        }
-
-        return sufixMax;
+        return total;
     }
 }
